d1ef954bf3bb208a9e5e29306996e0b8f2a01bf5
TechHero <eliogevisson.ladstone@gmail.com>
2018-01-26 16:54:45 -0300

    Like Clicker

--- jogos/LikeClicker/main.js
+++ jogos/LikeClicker/main.js
@@ -3,13 +3,20 @@ var likes = 0;
 var fans = 0;
 var superfans = 0;
 var megafans = 0;
+var ultrafans = 0;
 var bots = 0;
 var number = 1;
+var DPS = 0;
+var fanDPS = 1;
+var superFanDPS = 2;
+var megaFanDPS = 4;
+var ultraFanDPS = 8;
+var botDPS = 16;
 
 //BUG CORRECTION OF THE ENTER KEY
 window.addEventListener("keydown", keyDown);
 
-function keyDown(eevent) {
+function keyDown(event) {
 	if (window.event.keyCode == 13) 
 	{
 		event.returnValue=false; 
@@ -24,6 +31,8 @@ var doubleSuperFanCost = 1000;
 	document.getElementById('doubleSuperFanCost').innerHTML = doubleSuperFanCost;
 var doubleMegaFanCost = 6000;
 	document.getElementById('doubleMegaFanCost').innerHTML = doubleMegaFanCost;
+var doubleUltraFanCost = 12000;
+	document.getElementById('doubleUltraFanCost').innerHTML = doubleUltraFanCost;
 var doubleBotsCost = 25000;
 	document.getElementById('doubleBotsCost').innerHTML = doubleBotsCost;
 var doubleClickCost = 45000;
@@ -39,9 +48,11 @@ function resetGame(){
 	recreateDoubleBots();
 	recreateDoubleClick();
     likes = 0;
+	DPS = 0;
 	fans = 0;
 	superfans = 0;
 	megafans = 0;
+	ultrafans = 0;
 	bots = 0;
 	number = 1;
 	fanCost = 10;
@@ -50,11 +61,24 @@ function resetGame(){
 	doubleSuperFanCost = 1000;
 	megaFanCost = 500;
 	doubleMegaFanCost = 6000;
+	ultraFanCost = 1000;
+	doubleUltraFanCost = 12000;
 	botCost = 5000;
 	doubleBotCost = 25000;
 	addClickCost = 20000;
 	doubleClickCost = 45000;
 	updateLikes();
+	updateDPS();
+	fanDPS = 1;
+	document.getElementById('fanDPS').innerHTML = fanDPS;
+	superFanDPS = 2;
+	document.getElementById('superFanDPS').innerHTML = superFanDPS;
+	megaFanDPS = 4;
+	document.getElementById('megaFanDPS').innerHTML = megaFanDPS;
+	ultraFanDPS = 8;
+	document.getElementById('ultraFanDPS').innerHTML = ultraFanDPS;
+	botDPS = 16;
+	document.getElementById('botDPS').innerHTML = botDPS;
 	document.getElementById('fans').innerHTML = fans;
 	document.getElementById('fanCost').innerHTML = fanCost;
 	document.getElementById('doubleFanCost').innerHTML = doubleFanCost;
@@ -64,6 +88,9 @@ function resetGame(){
 	document.getElementById('megafans').innerHTML = megafans;
 	document.getElementById('megaFanCost').innerHTML = megaFanCost;
 	document.getElementById('doubleMegaFanCost').innerHTML = doubleMegaFanCost;
+	document.getElementById('ultrafans').innerHTML = ultrafans;
+	document.getElementById('ultraFanCost').innerHTML = ultraFanCost;
+	document.getElementById('doubleUltraFanCost').innerHTML = doubleUltraFanCost;
 	document.getElementById('bots').innerHTML = bots;
 	document.getElementById('botCost').innerHTML = botCost;
 	document.getElementById('doubleBotsCost').innerHTML = doubleBotCost;
@@ -75,7 +102,7 @@ function resetGame(){
 //CLICK FUNCTION
 function likeClick(number){
     likes = likes + number;
-	document.getElementById("likes").innerHTML = 'You have: ' + likes + ' likes!';
+	updateLikes();
 };
 
 //SHOP
@@ -87,10 +114,12 @@ function addFan(){
         fans = fans + 1;                                   //item number increases
     	likes = likes - fanCost;                          //spend the likes
         document.getElementById('fans').innerHTML = fans;  //updates the number of units purchased
-        updateLikes();                                     //update likes
+        updateLikes();                                    //update likes
+		DPS += fanDPS;                                    //increases the total dps according to item dps
+		updateDPS();                                     //update dps
     }
 	else {
-		notEnoughLikes();                                  //if you do not have enough likes, show an alert
+		notEnoughLikes();                                //if you do not have enough likes, show an alert
 	};
 	var nextCostFan = Math.floor(10 * Math.pow(1.1,fans));
     document.getElementById('fanCost').innerHTML = nextCostFan;  //update the number for the player
@@ -98,15 +127,13 @@ function addFan(){
 
 //x2 FANS
 function doubleFan(){
-    var doubleFanCost = 500;
+    var doubleFanCost = 500;                              //set the value of the item
     if(likes >= doubleFanCost){
-		fans = fans * 2;                                   //doubles the number of units of the respective item
     	likes = likes - doubleFanCost;
-		document.getElementById('fans').innerHTML = fans;  //updates the number of units purchased with x2
-		var nextCostFan = Math.floor(10 * Math.pow(1.1,fans));
-		document.getElementById('fanCost').innerHTML = nextCostFan; //updates the next cost
         updateLikes();
 		removeDoubleFan();
+		doubleFanDPS();
+		updateDPS();                                     //update dps
     }
 	else {
 		notEnoughLikes();
@@ -127,6 +154,16 @@ function recreateDoubleFan()                                 //"recreates" the x
    rctDoubleFan.style.visibility="initial"; 
 }
 
+function doubleFanDPS() {                                 //updates the fan dps
+	var doubleFansDPS = document.getElementById("DoubleFans");
+	if (doubleFansDPS.style.visibility == "hidden") {
+			fanDPS = 2;
+			updateDPS();
+		} else {
+			fanDPS = 1;
+		}
+}
+
 //+1 SUPER FAN
 function addSuperFan(){
     var superFanCost = Math.floor(100 * Math.pow(1.1,superfans));
@@ -134,7 +171,8 @@ function addSuperFan(){
         superfans = superfans + 1;
     	likes = likes - superFanCost;
         document.getElementById('superfans').innerHTML = superfans;
-        updateLikes();
+        DPS += superFanDPS;
+		updateDPS();
     }
 	else {
 		notEnoughLikes();
@@ -147,13 +185,11 @@ function addSuperFan(){
 function doubleSuperFan(){
     var doubleSuperFanCost = 1000;
     if(likes >= doubleSuperFanCost){
-		superfans = superfans * 2;
     	likes = likes - doubleSuperFanCost;
-		document.getElementById('superfans').innerHTML = superfans;
-		var nextCostSuperFan = Math.floor(100 * Math.pow(1.1,superfans));
-		document.getElementById('superFanCost').innerHTML = nextCostSuperFan;
         updateLikes();
 		removeDoubleSuperFan();
+		doubleSuperFanDPS();
+		updateDPS();
     }
 	else {
 		notEnoughLikes();
@@ -174,6 +210,16 @@ function recreateDoubleSuperFan()
    rctDoubleSuperFan.style.visibility="initial"; 
 }
 
+function doubleSuperFanDPS() {
+	var doubleSuperFansDPS = document.getElementById("DoubleSuperFans");
+	if (doubleSuperFansDPS.style.visibility == "hidden") {
+			superFanDPS = 4;
+			updateDPS();
+		} else {
+			superFanDPS = 2;
+		}
+}
+
 //+1 MEGA FAN
 function addMegaFan(){
     var megaFanCost = Math.floor(500 * Math.pow(1.1,megafans));
@@ -181,7 +227,8 @@ function addMegaFan(){
         megafans = megafans + 1;
     	likes = likes - megaFanCost;
         document.getElementById('megafans').innerHTML = megafans;
-        updateLikes();
+        DPS += megaFanDPS;
+		updateDPS();
     }
 	else {
 		notEnoughLikes();
@@ -192,15 +239,13 @@ function addMegaFan(){
 
 //x2 MEGA FANS
 function doubleMegaFan(){
-    var doubleSuperFanCost = 6000;
+    var doubleUltraFanCost = 6000;
     if(likes >= doubleMegaFanCost){
-		megafans = megafans * 2;
     	likes = likes - doubleMegaFanCost;
-		document.getElementById('megafans').innerHTML = megafans;
-		var nextCostMegaFan = Math.floor(500 * Math.pow(1.1,megafans));
-		document.getElementById('megaFanCost').innerHTML = nextCostMegaFan;
         updateLikes();
 		removeDoubleMegaFan();
+		doubleMegaFanDPS();
+		updateDPS();
     }
 	else {
 		notEnoughLikes();
@@ -221,6 +266,72 @@ function recreateDoubleMegaFan()
    rctDoubleMegaFan.style.visibility="initial"; 
 }
 
+function doubleMegaFanDPS() {
+	var doubleMegaFansDPS = document.getElementById("DoubleMegaFans");
+	if (doubleMegaFansDPS.style.visibility == "hidden") {
+			megaFanDPS = 8;
+			updateDPS();
+		} else {
+			megaFanDPS = 4;
+		}
+}
+
+//+1 ULTRA FAN
+function addUltraFan(){
+    var ultraFanCost = Math.floor(1000 * Math.pow(1.1,ultrafans));
+    if(likes >= ultraFanCost){
+        ultrafans = ultrafans + 1;
+    	likes = likes - ultraFanCost;
+        document.getElementById('ultrafans').innerHTML = ultrafans;
+        DPS += ultraFanDPS;
+		updateDPS();
+    }
+	else {
+		notEnoughLikes();
+	};
+    var nextCostUltraFan = Math.floor(1000 * Math.pow(1.1,ultrafans));
+    document.getElementById('ultraFanCost').innerHTML = nextCostUltraFan;
+};
+
+//x2 ULTRA FANS
+function doubleUltraFan(){
+    var doubleUltraFanCost = 12000;
+    if(likes >= doubleUltraFanCost){
+    	likes = likes - doubleUltraFanCost;
+        updateLikes();
+		removeDoubleUltraFan();
+		doubleUltraFanDPS();
+		updateDPS();
+    }
+	else {
+		notEnoughLikes();
+	};
+};
+
+function removeDoubleUltraFan() 
+{ 
+   var rmvDoubleUltraFan = document.getElementById("DoubleUltraFans"); 
+   rmvDoubleUltraFan.style.display="none"; 
+   rmvDoubleUltraFan.style.visibility="hidden"; 
+}
+
+function recreateDoubleUltraFan() 
+{ 
+   var rctDoubleUltraFan = document.getElementById("DoubleUltraFans"); 
+   rctDoubleUltraFan.style.display="block"; 
+   rctDoubleUltraFan.style.visibility="initial"; 
+}
+
+function doubleUltraFanDPS() {
+	var doubleUltraFansDPS = document.getElementById("DoubleUltraFans");
+	if (doubleUltraFansDPS.style.visibility == "hidden") {
+			ultraFanDPS = 16;
+			updateDPS();
+		} else {
+			ultraFanDPS = 8;
+		}
+}
+
 //+1 BOT
 function addBot(){
     var botCost = Math.floor(5000 * Math.pow(1.1,bots));
@@ -228,7 +339,8 @@ function addBot(){
         bots = bots + 1;
     	likes = likes - botCost;
         document.getElementById('bots').innerHTML = bots;
-        updateLikes();
+        DPS += botDPS;
+		updateDPS();
     }
 	else {
 		notEnoughLikes();
@@ -239,15 +351,13 @@ function addBot(){
 
 //x2 BOTS
 function doubleBots(){
-    var doubleBotsCost = 10000;
+    var doubleBotsCost = 25000;
     if(likes >= doubleBotsCost){
-		bots = bots * 2;
     	likes = likes - doubleBotsCost;
-		document.getElementById('bots').innerHTML = bots;
-		var nextCostBot = Math.floor(5000 * Math.pow(1.1,bots));
-		document.getElementById('botCost').innerHTML = nextCostBot;
         updateLikes();
 		removeDoubleBots();
+		doubleBotsDPS();
+		updateDPS();
     }
 	else {
 		notEnoughLikes();
@@ -268,6 +378,16 @@ function recreateDoubleBots()
    rctDoubleBots.style.visibility="initial"; 
 }
 
+function doubleBotsDPS() {
+	var doubleBotsDPS = document.getElementById("DoubleBots");
+	if (doubleBotsDPS.style.visibility == "hidden") {
+			botDPS = 32;
+			updateDPS();
+		} else {
+			botDPS = 16;
+		}
+}
+
 //+1 CLICK
 function addClick(){
     var addClickCost = Math.floor(20000 * Math.pow(1.1,number));
@@ -277,6 +397,7 @@ function addClick(){
         document.getElementById('number').innerHTML = number;
 		document.getElementById('addClickCost').innerHTML = nextCostAddClick;
         updateLikes();
+		updateDPC();
     }
 	else {
 		notEnoughLikes();
@@ -292,10 +413,11 @@ function doubleClick(){
         number = number * 2;
     	likes = likes - doubleClickCost;
         document.getElementById('number').innerHTML = number;
-		var nextCostBot = Math.floor(5000 * Math.pow(1.1,bots));
+		var nextCostAddClick = Math.floor(20000 * Math.pow(1.1,number));
 		document.getElementById('addClickCost').innerHTML = nextCostAddClick;
         updateLikes();
 		removeDoubleClick();
+		updateDPC();
     }
 	else {
 		notEnoughLikes();
@@ -343,16 +465,23 @@ function updateLikes(){
 	document.getElementById('likes').innerHTML = 'You have: ' + likes + ' likes!';
 }
 
+//FUNCTION OF UPDATING DPS
+function updateDPS(){
+	document.getElementById('dps').innerHTML = 'DPS: ' + DPS;
+	document.getElementById('fanDPS').innerHTML = fanDPS;
+	document.getElementById('superFanDPS').innerHTML = superFanDPS;
+	document.getElementById('megaFanDPS').innerHTML = megaFanDPS;
+	document.getElementById('ultraFanDPS').innerHTML = ultraFanDPS;
+	document.getElementById('botDPS').innerHTML = botDPS;
+}
+
+//FUNCTION OF UPDATING DPC
+function updateDPC(){
+	document.getElementById('dpc').innerHTML = 'DPC: ' + number;
+}
+
 //TIME INTERVAL (LIKES PER SECOND)
 window.setInterval(function(){
-	likeClick(fans);
-}, 10000);
-window.setInterval(function(){
-	likeClick(superfans);
+	likes = likes + DPS;
+	updateLikes();
 }, 1000);
-window.setInterval(function(){
-likeClick(megafans);
-}, 100);
-window.setInterval(function(){
-	likeClick(bots);
-}, 10);
