62241b4866af8cc6d6ef3fa2031c43ce583a70db
TechHero <eliogevisson.ladstone@gmail.com>
2018-05-23 00:17:48 -0300

    Hill Climb Racing HCR2 Mod

---
+++ hillclimbhcr2mod/files/script.js
@@ -0,0 +1,70 @@
+function openImage1(){
+	document.getElementById("scrsht1").style.visibility = "visible";
+	document.getElementById("scrsht1").style.display = "block";
+
+	document.getElementById("darkfullscreen").style.visibility = "visible";
+	document.getElementById("darkfullscreen").style.display = "block";
+}
+function closeImage1(){
+	document.getElementById("scrsht1").style.visibility = "hidden";
+	document.getElementById("scrsht1").style.display = "none";
+
+	document.getElementById("darkfullscreen").style.visibility = "hidden";
+	document.getElementById("darkfullscreen").style.display = "none";
+}
+function openImage2(){
+	document.getElementById("scrsht2").style.visibility = "visible";
+	document.getElementById("scrsht2").style.display = "block";
+
+	document.getElementById("darkfullscreen").style.visibility = "visible";
+	document.getElementById("darkfullscreen").style.display = "block";
+}
+function closeImage2(){
+	document.getElementById("scrsht2").style.visibility = "hidden";
+	document.getElementById("scrsht2").style.display = "none";
+
+	document.getElementById("darkfullscreen").style.visibility = "hidden";
+	document.getElementById("darkfullscreen").style.display = "none";
+}
+function openImage3(){
+	document.getElementById("scrsht3").style.visibility = "visible";
+	document.getElementById("scrsht3").style.display = "block";
+
+	document.getElementById("darkfullscreen").style.visibility = "visible";
+	document.getElementById("darkfullscreen").style.display = "block";
+}
+function closeImage3(){
+	document.getElementById("scrsht3").style.visibility = "hidden";
+	document.getElementById("scrsht3").style.display = "none";
+
+	document.getElementById("darkfullscreen").style.visibility = "hidden";
+	document.getElementById("darkfullscreen").style.display = "none";
+}
+function openImage4(){
+	document.getElementById("scrsht4").style.visibility = "visible";
+	document.getElementById("scrsht4").style.display = "block";
+
+	document.getElementById("darkfullscreen").style.visibility = "visible";
+	document.getElementById("darkfullscreen").style.display = "block";
+}
+function closeImage4(){
+	document.getElementById("scrsht4").style.visibility = "hidden";
+	document.getElementById("scrsht4").style.display = "none";
+
+	document.getElementById("darkfullscreen").style.visibility = "hidden";
+	document.getElementById("darkfullscreen").style.display = "none";
+}
+function openImage5(){
+	document.getElementById("scrsht5").style.visibility = "visible";
+	document.getElementById("scrsht5").style.display = "block";
+
+	document.getElementById("darkfullscreen").style.visibility = "visible";
+	document.getElementById("darkfullscreen").style.display = "block";
+}
+function closeImage5(){
+	document.getElementById("scrsht5").style.visibility = "hidden";
+	document.getElementById("scrsht5").style.display = "none";
+
+	document.getElementById("darkfullscreen").style.visibility = "hidden";
+	document.getElementById("darkfullscreen").style.display = "none";
+}
